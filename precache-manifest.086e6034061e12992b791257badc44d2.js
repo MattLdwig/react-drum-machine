b048c906968281526275d8a55e2d5c4a1966a387
MattLdwig <mattldwig@gmail.com>
2019-02-05 09:54:01 +0100

    Updates

---
+++ precache-manifest.086e6034061e12992b791257badc44d2.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f159b9017c410c4a742e40e29491156",
+    "url": "/react-drum-machine/static/media/A_sharp.9f159b90.wav"
+  },
+  {
+    "revision": "b3b87cc33a4e931c78bf",
+    "url": "/react-drum-machine/static/css/main.8e67d206.chunk.css"
+  },
+  {
+    "revision": "519308907992b0513c58",
+    "url": "/react-drum-machine/static/js/1.51930890.chunk.js"
+  },
+  {
+    "revision": "1b275f1ea04ecc9b85ff",
+    "url": "/react-drum-machine/static/js/runtime~main.1b275f1e.js"
+  },
+  {
+    "revision": "1b0068046bcd0881187266f49203376e",
+    "url": "/react-drum-machine/static/media/C.1b006804.wav"
+  },
+  {
+    "revision": "ada0031e68c024a680e40d8355ed69ec",
+    "url": "/react-drum-machine/static/media/C_sharp.ada0031e.wav"
+  },
+  {
+    "revision": "f25756904c9350e8cb6c5ea210f36321",
+    "url": "/react-drum-machine/static/media/D.f2575690.wav"
+  },
+  {
+    "revision": "968fc06aa7c4d7443aee95b927eb01db",
+    "url": "/react-drum-machine/static/media/D_sharp.968fc06a.wav"
+  },
+  {
+    "revision": "481b6810a050b8a7bfcfc7404af5261b",
+    "url": "/react-drum-machine/static/media/E.481b6810.wav"
+  },
+  {
+    "revision": "d2fcc9f34a818c30afb9eb231c92c249",
+    "url": "/react-drum-machine/static/media/F.d2fcc9f3.wav"
+  },
+  {
+    "revision": "d05b7b1d5ec46b70354a8110326e7348",
+    "url": "/react-drum-machine/static/media/F_sharp.d05b7b1d.wav"
+  },
+  {
+    "revision": "9ef3266ec498a25c8fb924c9da52e1bd",
+    "url": "/react-drum-machine/static/media/G.9ef3266e.wav"
+  },
+  {
+    "revision": "cf2133279e44a59615ad6e8d829ad6f0",
+    "url": "/react-drum-machine/static/media/G_sharp.cf213327.wav"
+  },
+  {
+    "revision": "ca065ab4c502473beba55d3cae6fb9c9",
+    "url": "/react-drum-machine/static/media/A.ca065ab4.wav"
+  },
+  {
+    "revision": "b3b87cc33a4e931c78bf",
+    "url": "/react-drum-machine/static/js/main.b3b87cc3.chunk.js"
+  },
+  {
+    "revision": "c02a00ed154a6f3c7f8f3a50c5e75f02",
+    "url": "/react-drum-machine/static/media/B.c02a00ed.wav"
+  },
+  {
+    "revision": "9ece8ab8438b99c33eef24ca56ceabb2",
+    "url": "/react-drum-machine/static/media/clap1.9ece8ab8.wav"
+  },
+  {
+    "revision": "f5426f33db8d7e1ac9ed0eddbf01075f",
+    "url": "/react-drum-machine/static/media/clap7.f5426f33.wav"
+  },
+  {
+    "revision": "c372b8c0622b95ba88d24a816de5f867",
+    "url": "/react-drum-machine/static/media/clap15.c372b8c0.wav"
+  },
+  {
+    "revision": "689949a429923807c44ea7f40dec7c7a",
+    "url": "/react-drum-machine/static/media/crash.689949a4.wav"
+  },
+  {
+    "revision": "57feed58b4a4c04a90daa0cde9d15440",
+    "url": "/react-drum-machine/static/media/crash2.57feed58.wav"
+  },
+  {
+    "revision": "e3028c2389817ffb9da090b0ec51edc2",
+    "url": "/react-drum-machine/static/media/hithat.e3028c23.wav"
+  },
+  {
+    "revision": "bd6321f48c56fe494ef2c23889796db8",
+    "url": "/react-drum-machine/static/media/openhat.bd6321f4.wav"
+  },
+  {
+    "revision": "30c5cba2eb2e8e73b26c98143cf9f79d",
+    "url": "/react-drum-machine/static/media/kick.30c5cba2.wav"
+  },
+  {
+    "revision": "8a27334b948adc1fe0e3621134867449",
+    "url": "/react-drum-machine/static/media/shaker.8a27334b.wav"
+  },
+  {
+    "revision": "4387a0752a31abd2d14d9cae0e80f577",
+    "url": "/react-drum-machine/static/media/snare3.4387a075.wav"
+  },
+  {
+    "revision": "44d0ce345f950eee56663b85b536dfac",
+    "url": "/react-drum-machine/static/media/snare8.44d0ce34.wav"
+  },
+  {
+    "revision": "9263ece4aa7b1c2e9893668e9c0f6030",
+    "url": "/react-drum-machine/static/media/snare9.9263ece4.wav"
+  },
+  {
+    "revision": "2e498b759805dc059fc2eaef99d69cf3",
+    "url": "/react-drum-machine/index.html"
+  }
+];
